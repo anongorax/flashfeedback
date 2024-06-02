90937f15a4dd37aabba9432b41f375b21cce6841
anongorax <harshdeepsaiyam@gmail.com>
2024-06-02 19:17:59 +0530

    remove chakra ui dep

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ProvideAuth } from "@/lib/auth";
+import { Providers } from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ProvideAuth>
+      <Providers>
         <body className={inter.className}>{children}</body>
-      </ProvideAuth>
+      </Providers>
     </html>
   );
 }
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,13 +1,8 @@
 'use client'
-
 import { ProvideAuth } from '@/lib/auth'
-import { ChakraProvider } from '@chakra-ui/react'
-import theme from '@/styles/Theme'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-    return <ChakraProvider theme={theme}>
-        <ProvideAuth>
-            {children}
-        </ProvideAuth>
-    </ChakraProvider>
+    return <ProvideAuth>
+        {children}
+    </ProvideAuth>
 }
